9e09e83247b825ab602eb626ec26944f0ef347c1
Rosalie Chan <rosaliechan@wirelessprvnat-172-17-12-70.near.illinois.edu>
2016-02-20 14:29:55 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/karisir/rude/MainActivity.java

--- app/src/main/java/com/example/karisir/rude/MainActivity.java
+++ app/src/main/java/com/example/karisir/rude/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.karisir.rude;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends Activity {
 
@@ -13,13 +28,18 @@ public class MainActivity extends Activity {
 
     private ProgressBar mProgress;
     private int mProgressStatus = 0;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
 
     private Handler mHandler = new Handler();
 
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //progress bar
+        super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.progressbar_activity);
+        setContentView(R.layout.activity_main);
 
         mProgress = (ProgressBar) findViewById(R.id.progress_bar);
 
@@ -27,7 +47,7 @@ public class MainActivity extends Activity {
         new Thread(new Runnable() {
             public void run() {
                 while (mProgressStatus < 100) {
-                    mProgressStatus = doWork();
+                   // mProgressStatus = doWork();
 
                     // Update the progress bar
                     mHandler.post(new Runnable() {
@@ -38,44 +58,69 @@ public class MainActivity extends Activity {
                 }
             }
         }).start();
+<<<<<<< HEAD
     }
-=======
 
-public class MainActivity extends ActionBarActivity {
+=======
 >>>>>>> origin/master
 
-    private boolean loggedin = false;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        loggedin = getIntent().getExtras().getBoolean("loggedin");
-        if(!loggedin) {
-            Intent in = new Intent(this, FacebookLogin.class);
-            startActivity(in);
+
+           //facebook integration
+            FacebookSdk.sdkInitialize(this.getApplicationContext());
+            //setContentView(R.layout.activity_login);
+            callbackManager = CallbackManager.Factory.create();
+            loginButton = (LoginButton) findViewById(R.id.login_button);
+            List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+            loginButton.setReadPermissions(permissionNeeds);
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+            {
+                @Override
+                public void onSuccess(LoginResult loginResult)
+                {
+                    System.out.println("onSuccess");
+                }
+
+                @Override
+                public void onCancel()
+                {
+                    System.out.println("onCancel");
+                }
+
+                @Override
+                public void onError(FacebookException exception)
+                {
+                    Log.v("LoginActivity", exception.getCause().toString());
+                }
+            });
         }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_facebook_login, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-}
+
+    }
