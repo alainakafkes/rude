6d290b9937af671b9c205a595a5b4701ddc4234a
Rosalie Chan <rosaliechan@wirelessprvnat-172-17-12-70.near.illinois.edu>
2016-02-20 01:57:45 -0600

    changed toggle

--- app/src/main/java/com/example/karisir/rude/MainActivity.java
+++ app/src/main/java/com/example/karisir/rude/MainActivity.java
@@ -5,13 +5,49 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.facebook.FacebookSdk;
+import android.app.Activity;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private TextView switchStatus;
+    private Switch mySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchStatus = (TextView) findViewById(R.id.switchStatus);
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
+
+        //set the switch to ON
+        mySwitch.setChecked(true);
+        //attach a listener to check for changes in state
+        mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if (isChecked) {
+                    switchStatus.setText("Switch is currently ON");
+                } else {
+                    switchStatus.setText("Switch is currently OFF");
+                }
+
+            }
+        });
+
+        //check the current state before we display the screen
+        if (mySwitch.isChecked()) {
+            switchStatus.setText("Switch is currently ON");
+        } else {
+            switchStatus.setText("Switch is currently OFF");
+        }
     }
 
     @Override
@@ -20,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -36,3 +73,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
