c3375d4957b22293a1ce3b126e438e05459fd53b
Alaina Kafkes <alainakafkes2017@u.northwestern.edu>
2016-02-20 18:11:35 -0600

    Added touch screen detection

---
+++ app/src/main/java/com/example/karisir/rude/FacebookLogin.java
@@ -0,0 +1,100 @@
+package com.example.karisir.rude;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FacebookLogin extends ActionBarActivity {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        //setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+        loginButton.setReadPermissions(permissionNeeds);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
+        {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                System.out.println("onSuccess");
+            }
+
+            @Override
+            public void onCancel()
+            {
+                System.out.println("onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception)
+            {
+                Log.v("LoginActivity", exception.getCause().toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_facebook_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+
+        try {
+            Intent in = new Intent(this, MainActivity.class);
+            in.putExtra("loggedin", true);
+            startActivity(in);
+        }
+        catch (Exception e){
+            Log.i("tag","Failed to launch Settings Activity " + e.getMessage());
+        }
+    }
+
+        }
+
+
--- app/src/main/java/com/example/karisir/rude/MainActivity.java
+++ app/src/main/java/com/example/karisir/rude/MainActivity.java
@@ -1,23 +1,138 @@
 package com.example.karisir.rude;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private static final int PROGRESS = 0x1;
+
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+
+    private Handler mHandler = new Handler();
+
+
+    private boolean isTouch = false; // touch screen detection
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //progress bar
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 100) {
+                    // mProgressStatus = doWork();
+
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus);
+                        }
+                    });
+                }
+            }
+        }).start();
+
+
+        //facebook integration
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        //setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
+        loginButton.setReadPermissions(permissionNeeds);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("onSuccess");
+            }
+
+            @Override
+            public void onCancel() {
+                System.out.println("onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.v("LoginActivity", exception.getCause().toString());
+            }
+        });
     }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int X = (int) event.getX();
+        int Y = (int) event.getY();
+
+        int eventaction = event.getAction();
+
+        switch (eventaction) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                Toast.makeText(this, "ACTION_DOWN AT COORDS "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+
+                isTouch = true;
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                Toast.makeText(this, "MOVE "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                Toast.makeText(this, "ACTION_UP "+"X: "+X+" Y: "+Y, Toast.LENGTH_SHORT).show();
+
+                break;
+
+        }
+
+        return true;
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_facebook_login, menu);
         return true;
     }
 
@@ -34,5 +149,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
